01ad38acaf2259af786cca51a147c69a310e9273
123 <314040307@qq.com>
2016-01-13 17:13:44 +0800

    fd
    xixi

---
+++ product.js
@@ -0,0 +1,84 @@
+/**
+ * Created by Administrator on 2016/1/13.
+ */
+var ProductCatagoryRow=React.createClass({
+  render:function(){
+    return(<tr><th colSpan="2">{this.props.category}</th></tr>);
+  }
+});
+var ProductRow=React.createClass({
+  render:function(){
+    var name=this.props.product.stocked?
+      this.props.product.name:
+      <span style={{color:'red'}}>
+        {this.props.product.name}
+        </span>;
+    return(
+      <tr>
+        <td>{name}</td>
+        <td>{this.props.product.price}</td>
+      </tr>
+    );
+    }
+});
+var ProductTable=React.createClass({
+  render:function(){
+    var rows=[];
+    var lastCategory=null;
+    this.props.products.forEach(function(product){
+      if(product.category!==lastCategory){
+        rows.push(<ProductCatagoryRow category={product.category} key={product.category} />);
+      }
+      rows.push(<ProductRow product={product} key={product.name}/>);
+      lastCategory=product.category;
+    });
+    return(
+      <table>
+        <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    );
+  }
+});
+var SearchBar=React.createClass({
+  render: function () {
+    return(
+      <form>
+        <input type="text" placeholder="Search..."/>
+        <p>
+          <input type="checkbox"/>
+          {''}
+          Only Show Products in Stock
+        </p>
+      </form>
+    );
+  }
+});
+var FilterableProductTable=React.createClass({
+  render: function () {
+    return(
+      <div>
+        <SearchBar/>
+        <FilterableProductTable products={this.props.products}/>
+      </div>
+    );
+  }
+});
+var array = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
+ReactDOM.render(
+  <FilterableProductTable products={PRODUCTS}/>,
+  document.getElementById('content')
+);
+
